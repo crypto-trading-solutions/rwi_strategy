794eb71b24099a55ed1d7229b465fd3129bd1e73
DmitriyShalaev <shalaevdmitriy99@gmail.com>
2020-06-09 21:33:10 +0300

    init binance node api

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
 
+const http = require('http');
+
+const Binance = require('node-binance-api');
+const binance = new Binance().options({
+  APIKEY: process.env.APIKEY,
+  APISECRET:  process.env.APISECRET
+});
+
+//check connection to binance 
+binance.futuresBalance().then( (data) => {
+  console.info(data);
+});
 
 const port = process.env.PORT || 4040;
 const ipAddress = process.env.IP_ADDRESS;
-const http = require('http');
+
 const handleHttpServerErrors = require('./utils/handleHttpServerErrors');
 
 var express = require('express'),
